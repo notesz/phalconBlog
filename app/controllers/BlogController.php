d6669b05820a3cd52ee57c94ce395f8bd9907bf3
notesz <notesz@notesz.net>
2015-06-14 22:35:23 +0200

    Posts order

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -1,16 +1,41 @@
 <?php
 
+use Phalcon\Paginator\Adapter\NativeArray as PaginatorArray;
+use Phalcon\Http\Request;
+
 class BlogController extends ControllerBase
 {
 
     protected $blog;
 
+    protected $request;
+
     public function initialize() {
         $this->blog = new Blog();
+        $this->request = new Request();
     }
 
     public function indexAction() {
-        $this->view->setVar('posts', $this->blog->getPost());
+        $posts = $this->blog->getPost();
+
+        $paginator = new PaginatorArray(
+            array(
+                'data'  => $posts,
+                'limit' => 10,
+                'page'  => $this->request->get('page')
+            )
+        );
+
+        $this->view->setVar('posts', $paginator->getPaginate());
+
+        $this->view->setVar('pager', array(
+            'before'      => $paginator->getPaginate()->before,
+            'next'        => $paginator->getPaginate()->next,
+            'first'       => $paginator->getPaginate()->first,
+            'last'        => $paginator->getPaginate()->last,
+            'current'     => $paginator->getPaginate()->current,
+            'total_pages' => $paginator->getPaginate()->total_pages,
+        ));
     }
 
     public function postAction() {
